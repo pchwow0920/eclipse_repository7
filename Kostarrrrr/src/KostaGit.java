346953aca9bd0ffe7f3c9ba853a513297c530b87
pchwow <pchwow@naver.com>
2023-10-06 17:37:53 +0900

    initial commit

---
+++ Kostarrrrr/src/KostaGit.java
@@ -0,0 +1,13 @@
+
+public class KostaGit {
+
+	public static void main(String[] args) {
+		System.out.println("KostaGit");
+		System.out.println("KostaGit2");
+		System.out.println("KostaGit3");
+		System.out.println("KostaGit4");
+		System.out.println("KostaGit5");
+		System.out.println("KostaGit6");
+	}
+
+}
